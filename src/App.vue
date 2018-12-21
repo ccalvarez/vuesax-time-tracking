f632835646fba92b64bcd41e04d45a4af4d9e306
Carolina Cordero <ccalvarez@gmail.com>
2018-12-21 15:57:05 -0600

    feat: muestra lista de tareas. Probando vuesax List

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <!-- <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>-->
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <vs-button vs-type="filled">Primary</vs-button>
-  </div>
+  <vs-list>
+    <vs-list-header title="En ejecución"></vs-list-header>
+    <vs-list-item
+      v-for="(task, index) in tareas"
+      :key="index"
+      :title="task.description"
+      :subtitle="task.project.name"
+    ></vs-list-item>
+    <vs-list-header title="Pausadas" color="success"></vs-list-header>
+    <vs-list-item title="Enid's" subtitle="At night a bar, during the day a delicious brunch spot."></vs-list-item>
+    <vs-list-item title="Veronika Ossi" subtitle="Has not watched anything recently"></vs-list-item>
+  </vs-list>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+import 'vuesax/dist/vuesax.css'; //Vuesax styles
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  data() {
+    return {
+      tareas: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(process.env.VUE_APP_APIURL)
+      .then(response => {
+        if (response.status === 200) {
+          this.tareas = response.data;
+        } else if (response.status === 204) {
+        }
+      })
+      .catch(error => {});
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
