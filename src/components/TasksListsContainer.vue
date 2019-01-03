0c7e926602e088091aae14ea0a51f8de1eecd639
Carolina Cordero <ccalvarez@gmail.com>
2019-01-03 15:52:55 -0600

    [WIP] feat: agrega botón Nueva tarea. #7

--- src/components/TasksListsContainer.vue
+++ src/components/TasksListsContainer.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <tasks-list state="running" :tasks="runningTasks"></tasks-list>
-    <tasks-list state="paused" :tasks="pausedTasks"></tasks-list>
-    <tasks-list state="pending" :tasks="pendingTasks"></tasks-list>
-    <tasks-list state="finished" :tasks="finishedTasks"></tasks-list>
+    <vs-row id="content-header">
+      <vs-col>
+        <vs-button class="addButton" color="success" size="large" @click="addTask()">Nuevo Sistema</vs-button>
+      </vs-col>
+    </vs-row>
+    <div id="lists-container">
+      <tasks-list state="running" :tasks="runningTasks"></tasks-list>
+      <tasks-list state="paused" :tasks="pausedTasks"></tasks-list>
+      <tasks-list state="pending" :tasks="pendingTasks"></tasks-list>
+      <tasks-list state="finished" :tasks="finishedTasks"></tasks-list>
+    </div>
   </div>
 </template>
 <script>
@@ -11,6 +18,9 @@ import { mapGetters } from 'vuex';
 import TasksList from './TasksList.vue';
 
 export default {
+  mounted() {
+    // document.getElementById('content-header').style.display = 'inline-flex';
+  },
   components: {
     tasksList: TasksList,
   },
@@ -25,4 +35,17 @@ export default {
 };
 </script>
 <style>
+#content-header {
+  margin-top: 20px;
+}
+.addButton {
+  float: right;
+  /* width: 100%;
+  text-align: right;
+  margin: 0 auto; */
+}
+#lists-container {
+  margin-top: 10px;
+  /* padding: 5px; */
+}
 </style>
