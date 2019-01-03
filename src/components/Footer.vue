2b6655db9ee529e23d2f123b7c99dc9d714addd5
Carolina Cordero <ccalvarez@gmail.com>
2019-01-03 12:46:15 -0600

    chore: comenta temporalmente Source code on

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <div class="center">
-      <p>Made with &nbsp;
+      <p>
+        Made with &nbsp;
         <a href="https://vuejs.org/" target="_blank">
           <img src="../assets/logo.png" alt="Vue.js" width="12" height="12">
         </a> &nbsp; &
         <a href="https://expressjs.com/" target="_blank">Express</a>
       </p>
-      <p>Source code on &nbsp;
+      <!-- <p>Source code on &nbsp;
         <a href="https://github.com/ccalvarez/vuesax-time-tracking" target="_blank">
           <img src="../assets/GitHub-Mark-32px.png" alt="GitHub" width="12" height="12">
         </a>
-      </p>
+      </p>-->
     </div>
   </div>
 </template>
