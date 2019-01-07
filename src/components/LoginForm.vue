490a6a366b6d60ffb9cab9641da2b8594da4c78c
Carolina Cordero <ccalvarez@gmail.com>
2019-01-07 08:44:27 -0600

    feat: agrega ruta /login y control de Login

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vs-input type="email" label="Email" v-model="email"></vs-input>
+    <br>
+    <vs-input type="password" label="Password" v-model="password"></vs-input>
+    <br>
+    <vs-button size="large" @click="login()">Acceder</vs-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+};
+</script>
+<style>
+</style>
